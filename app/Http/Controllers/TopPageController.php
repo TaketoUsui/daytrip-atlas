8bed228e9325eac71c29ce146a6694edd399a05a
TaketoUsui <take20030524@yahoo.co.jp>
2025-10-28 01:28:29 +0900

    [feat]トップページのコントローラー

--- app/Http/Controllers/TopPageController.php
+++ app/Http/Controllers/TopPageController.php
@@ -7,18 +7,25 @@ use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Inertia\Response as InertiaResponse;
 
-/**
- * 【FUNC-001】トップページ表示
- */
 class TopPageController extends Controller
 {
-    public function show(Request $request): InertiaResponse{
+    /**
+     * No.1 トップページ表示
+     * ユーザーの現在地入力と興味のあるタグ選択を受け付ける
+     *
+     * @param Request $request
+     * @return InertiaResponse
+     */
+    public function show(Request $request): InertiaResponse
+    {
         $tags = Tag::query()
-            ->select(["id", "name"])
+            ->select(['id', 'name'])
+            ->orderBy('id', 'asc')
+            ->limit(10)
             ->get();
 
-        return Inertia::render("TopPage", [
-            "tags" => $tags,
+        return Inertia::render('TopPage', [
+            'tags' => $tags,
         ]);
     }
 }
