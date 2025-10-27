e6cdc24f82d30408ce3d200fb61526cc83064f33
TaketoUsui <take20030524@yahoo.co.jp>
2025-10-27 19:55:46 +0900

    [feat]ルーティングを設定

--- routes/api.php
+++ routes/api.php
@@ -2,17 +2,21 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\V1\SpotInterestController;
+use App\Http\Controllers\Api\V1\UserSpotInterestController;
 use App\Http\Controllers\Api\V1\SuggestionStatusController;
 use App\Http\Controllers\Api\V1\UserActionLogController;
 
-Route::prefix("v1")->group(function () {
-    Route::get("/suggestions/{uuid}/status", [SuggestionStatusController::class, "show"])
-        ->name("api.v1.suggestion.status");
+Route::prefix('v1')->group(function () {
 
-    Route::post("/spots/{spot}/interest", [SpotInterestController::class, "store"])
-        ->name("api.v1.spots.interest");
+    // No.6 提案ステータス取得
+    Route::get('/suggestions/{suggestion_set:uuid}/status', [SuggestionStatusController::class, 'show'])
+        ->name('api.v1.suggestions.status');
 
-    Route::post("/logs/action", [UserActionLogController::class, "store"])
-        ->name("api.v1.logs.action");
+    // No.7 明示的フィードバック送信
+    Route::post('/spots/{spot:id}/interest', [UserSpotInterestController::class, 'store'])
+        ->name('api.v1.spots.interest');
+
+    // No.8 ユーザー行動ログ記録
+    Route::post('/logs/action', [UserActionLogController::class, 'store'])
+        ->name('api.v1.logs.action');
 });
--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,52 @@
 <?php
 
+use App\Http\Controllers\Admin\AdminTopPageController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TopPageController;
-use App\Http\Controllers\ClusterController;
 use App\Http\Controllers\SuggestionController;
+use App\Http\Controllers\SuggestionPageController;
+use App\Http\Controllers\SuggestionItemPageController;
+use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\ClusterController;
+use App\Http\Controllers\Admin\SpotController;
+use App\Http\Controllers\Admin\TagController;
 
-Route::get("/", [TopPageController::class, "show"])
-    ->name("top");
+// No.1 トップページ
+Route::get('/', [TopPageController::class, 'show'])
+    ->name('top');
 
-Route::post("/suggestions", [SuggestionController::class, "store"])
-    ->name("suggestions.store");
+// No.2 提案リクエスト受付
+Route::post('/suggestions', [SuggestionController::class, 'store'])
+    ->name('suggestions.store');
 
-Route::get("/suggestions/{uuid}/wait", [SuggestionController::class, "wait"])
-    ->name("suggestions.wait");
+// No.3 提案待機ページ
+Route::get('/suggestions/{suggestion_set:uuid}/wait', [SuggestionPageController::class, 'wait'])
+    ->name('suggestions.wait');
 
-Route::get("/suggestions/{uuid}", [SuggestionController::class, "show"])
-    ->name("suggestions.show");
+// No.4 提案結果一覧
+Route::get('/suggestions/{suggestion_set:uuid}', [SuggestionPageController::class, 'show'])
+    ->name('suggestions.show');
 
-Route::get("/clusters/{suggestion_set_item_uuid}", [ClusterController::class, "show"])
-    ->name("clusters.show");
+// No.5 観光地域詳細
+Route::get('/suggested-cluster/{suggestion_set_item:uuid}', [SuggestionItemPageController::class, 'show'])
+    ->name('suggestions.item.show');
+
+Route::middleware(['auth'])
+->prefix('admin')
+->name('admin.')
+->group(function () {
+    Route::get("/", [AdminTopPageController::class, "show"])
+        ->name('top');
+
+    // スポット管理 (CRUD)
+    Route::resource('spots', SpotController::class);
+
+    // クラスター管理 (CRUD)
+    Route::resource('clusters', ClusterController::class);
+
+    // タグ管理 (CRUD)
+    Route::resource('model-plans', TagController::class);
+
+    // カテゴリ管理 (CRUD)
+    Route::resource('model-plans', CategoryController::class);
+});
